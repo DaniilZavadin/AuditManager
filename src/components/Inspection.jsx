1f460352ea23dfd9242f5132e9d9fbbf027dfbc9
daniil.zavadin <daniil.zavadin@gmail.com>
2021-12-13 00:05:25 +0200

    Structure set up, first steps

---
+++ src/components/Inspection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inspection = ({name, photo, deatils,}) => {
+    return (
+        <div className='inspection-item'>
+            
+        </div>
+    );
+};
+
+export default Inspection;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+// import React from 'react';
+// import { Route, Switch, Redirect } from 'react-router-dom';
+// import Authorization from '../pages/Authorization';
+// import InspectionsList from '../pages/InspectionsList'; 
+// import InspectionDetails from '../pages/InspectionDetails'; 
+// import InspectionEdit from '../pages/InspectionEdit'; 
+
+// const AppRoutes = () => {
+//   return (
+//     <Switch>
+//       <Redirect exact from="/" to="/main" />
+//       <Route path="/authorization" render={() => <Authorization />} />
+//       <Route path="/inspections" render={() => <InspectionsList />} />
+//       <Route path="/inspection_details" render={() => <InspectionDetails />} />
+//       <Route path="/edit" render={() => <InspectionEdit />} />
+//       <Route path="*" render={() => <div>404</div>} />
+//     </Switch>
+//   );
+// };
+
+// export default AppRoutes;
