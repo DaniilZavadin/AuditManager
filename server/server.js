04df9166065088e0ff4b264cdef20b9a33438d40
daniil.zavadin <daniil.zavadin@gmail.com>
2022-01-14 21:03:15 +0200

    backup to image upload, begin work on json creatinon

--- server/server.js
+++ server/server.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const fs = require('fs');
 
 const app = express();
 app.use(cors());
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'public')
-    },
-    filename: (req, file, callback) => {
-        callback(null, Date.now() + '-' + file.originalname)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null, 'public')
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, Date.now() + '-' + file.originalname)
+//     }
+// })
 
-const upload = multer({storage}).single('file');
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) return res.status(500).json(err)
-        return res.status(200).send(req.file)
-    })
+// const upload = multer({ storage }).single('file');
+// app.post('/uploadfile', (req, res) => {
+//     upload(req, res, (err) => {
+//         if (err) return res.status(500).json(err)
+//         return res.status(200).send(req.file)
+//     })
+// })
+
+app.post('/uploadjson', (req, res) => {
+    let name = 'data.json';
+    var init = JSON.parse(fs.readFileSync(name).toString());
+    init = { ...init, 'prikol': true }
+    fs.writeFileSync(name, JSON.stringify(init));
 })
 
 app.listen(8000, () => {
